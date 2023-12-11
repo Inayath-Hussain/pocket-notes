e6f1855be98cce7bed4ecd2f2af16aad46fd666f
Inayath <inayathh2@gmail.com>
2023-12-11 12:19:11 +0530

    Fix: scrollbar in navbar and breakpoint for laptop
    fixed scrollbar design in navbar and changed breakpoint for desktops for better UI in tablet size devices.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -19,11 +19,11 @@ const testData: Igroup[] = [
 const NavBar = () => {
 
     const [groups, setGroups] = useState<Igroup[]>(testData);
+    const [currentGroup, setCurrentGroup] = useState("");
 
     const { pathname } = useLocation();
-    const { showNewGroupModal, NewGroupFormPortal } = useNewGroupModal();
 
-    console.log(pathname)
+    const { showNewGroupModal, NewGroupFormPortal } = useNewGroupModal();
 
     const shouldDisplay = pathname === homeRoute
     console.log(shouldDisplay)
@@ -33,13 +33,87 @@ const NavBar = () => {
 
             <h1>Pocket Notes</h1>
 
-            {/* map icons and grpName */}
-            {groups.length > 0 && groups.map(g => (
-                <Link to={g.groupName} key={g.groupName} className="link">
-                    <NavIcon groupName={g.groupName} bgColor={g.bgColor} />
-                    <p>{g.groupName}</p>
+            <div className="links-container">
+                {/* map icons and grpName */}
+                {groups.length > 0 && groups.map(g => (
+
+                    <Link to={g.groupName} key={g.groupName} onClick={() => setCurrentGroup(g.groupName)}
+                        className={`link ${g.groupName === currentGroup ? "current" : ""}`}>
+
+                        <NavIcon groupName={g.groupName} bgColor={g.bgColor} />
+                        <p>{g.groupName}</p>
+
+                    </Link>
+
+                ))}
+
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
                 </Link>
-            ))}
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+                <Link to={"/"} onClick={() => setCurrentGroup("React Native Notes")}
+                    className={`link ${"React Native Notes" === currentGroup ? "current" : ""}`}>
+
+                    <NavIcon groupName={"React Native Notes"} bgColor={"red"} />
+                    <p>{"React Native Notes"}</p>
+
+                </Link>
+            </div>
+
+
+
 
             <button onClick={() => { showNewGroupModal(); console.log("hello ...") }} className="add-grp-btn">
                 &#43;
